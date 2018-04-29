e8627349d75de83e03ad4f2c14c0215847623841
shohame <33593461+shohame@users.noreply.github.com>
2018-04-29 14:57:59 +0300

    Update try.c

--- Arduino1/try.c
+++ Arduino1/try.c
@@ -1,7 +1,15 @@
 
 
-void Foo(int a)
+void setup()
 {
-	return;
-	
+   pinMode(8,  OUTPUT); // initialize digital pin 8 as an output.
+}
+
+// the loop function runs over and over again forever
+void loop()
+{
+  digitalWrite(8, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(100);                       // wait for a second
+  digitalWrite(8, LOW);    // turn the LED off by making the voltage LOW
+  delay(100);                       // wait for a second
 }
